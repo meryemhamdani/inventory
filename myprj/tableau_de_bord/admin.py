4f3d34fc9047e3a0aa5dd9f779a6f167a05bed10
hamda <hamdanimeryem2002@gmail.com>
2023-02-21 22:06:20 +0100

    v6

--- myprj/tableau_de_bord/admin.py
+++ myprj/tableau_de_bord/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product ,Order
+admin.site.site_header = 'Tableau de bord Gestion de stock'
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
