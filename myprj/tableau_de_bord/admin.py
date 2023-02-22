447f407f3686efc046e35ece1d0e3b5a8a6884a0
hamda <hamdanimeryem2002@gmail.com>
2023-02-22 13:14:04 +0100

    v7#

--- myprj/tableau_de_bord/admin.py
+++ myprj/tableau_de_bord/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product ,Order
+from .models import Product
+#from django.contrib.auth.models import Group
 admin.site.site_header = 'Tableau de bord Gestion de stock'
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity',)
+    list_filter = ['category']
+admin.site.register(Product, ProductAdmin)
--- myprj/tableau_de_bord/views.py
+++ myprj/tableau_de_bord/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'tableau_de_bord/index.html')
 
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    return render(request, 'tableau_de_bord/staff.html')
 
 def product(request):
-    return render(request, 'dashboard/product.html')
+    return render(request, 'tableau_de_bord/product.html')
 
 def order(request):
-    return render(request, 'dashboard/order.html')
+    return render(request, 'tableau_de_bord/order.html')
 
