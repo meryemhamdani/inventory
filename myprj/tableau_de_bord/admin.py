e723648c42d6b37b62eaac77bbcc3ce3c8c56b76
pc <chaymaebouali13@gmail.com>
2023-02-23 22:31:28 +0100

    commit1

--- myprj/tableau_de_bord/admin.py
+++ myprj/tableau_de_bord/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Product ,Order
+from .models import Product
+from django.contrib.auth.models import Group
+
+admin.site.site_header = "Gestion de stock dashboard"
 admin.site.site_header = 'Tableau de bord Gestion de stock'
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity')
+    list_filter = ['category',]
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
+#admin.site.unregister(Group)
